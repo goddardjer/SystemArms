2ee8a375901f5e1999b3e6dafb928d8d7cb10641
goddardjer <goddarje@oregonstate.edu>
2024-02-28 12:50:35 -0800

    test

--- Position_testin.py
+++ Position_testin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # coding=utf8
 import sys
+import argparse
 sys.path.append('/home/pi/ArmPi/')
 import time
 from ArmIK.ArmMoveIK import *
@@ -8,21 +9,23 @@ import HiwonderSDK.Board as Board
 
 AK = ArmIK()
 
-# Global variable for the target position
-target_position = (0, 0, 0)
-
 # Function to move the arm to the target position
-def move_to_target():
-    global target_position
-    x, y, z = target_position
-    result = AK.setPitchRangeMoving((x, y, z), -90, -90, 0)
+def move_to_target(x, y, z, pitch1, pitch2):
+    result = AK.setPitchRangeMoving((x, y, z), pitch1, pitch2, 1000)
     if result == False:
         print("Unreachable")
     else:
         time.sleep(result[2]/1000)
 
 if __name__ == '__main__':
-    # Set the target position
-    target_position = (-15 + 0.5, 12 - 0.5, 1.5)
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Move the robotic arm to a specified position.')
+    parser.add_argument('x', type=float, help='X coordinate')
+    parser.add_argument('y', type=float, help='Y coordinate')
+    parser.add_argument('z', type=float, help='Z coordinate')
+    parser.add_argument('pitch1', type=int, help='Pitch 1')
+    parser.add_argument('pitch2', type=int, help='Pitch 2')
+    args = parser.parse_args()
+
     # Move the arm to the target position
-    move_to_target()
+    move_to_target(args.x, args.y, args.z, args.pitch1, args.pitch2)
