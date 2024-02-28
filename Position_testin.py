820f1299816ba6021aa2362fd09eaf57105163ef
goddardjer <goddarje@oregonstate.edu>
2024-02-28 13:14:29 -0800

    sdf

--- Position_testin.py
+++ Position_testin.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 # coding=utf8
+
 import sys
-import argparse
 sys.path.append('/home/pi/ArmPi/')
+
+import cv2
 import time
+import Camera
+import threading
+import math
+from LABConfig import *
+from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+
+servo1 = 500
 
 AK = ArmIK()
 
@@ -19,13 +29,18 @@ def move_to_target(x, y, z, pitch1, pitch2):
 
 if __name__ == '__main__':
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Move the robotic arm to a specified position.')
-    parser.add_argument('x', type=float, help='X coordinate')
-    parser.add_argument('y', type=float, help='Y coordinate')
-    parser.add_argument('z', type=float, help='Z coordinate')
-    parser.add_argument('pitch1', type=int, help='Pitch 1')
-    parser.add_argument('pitch2', type=int, help='Pitch 2')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Move the robotic arm to a specified position.')
+    # parser.add_argument('x', type=float, help='X coordinate')
+    # parser.add_argument('y', type=float, help='Y coordinate')
+    # parser.add_argument('z', type=float, help='Z coordinate')
+    # parser.add_argument('pitch1', type=int, help='Pitch 1')
+    # parser.add_argument('pitch2', type=int, help='Pitch 2')
+    # args = parser.parse_args()
+
+    Board.setBusServoPulse(1, servo1 - 280, 500)
+    servo2_angle = getAngle(0, 0, 0)
+    Board.setBusServoPulse(2, servo2_angle, 500)
+    time.sleep(0.8)
 
     # Move the arm to the target position
-    move_to_target(args.x, args.y, args.z, args.pitch1, args.pitch2)
+    #move_to_target(args.x, args.y, args.z, args.pitch1, args.pitch2)
