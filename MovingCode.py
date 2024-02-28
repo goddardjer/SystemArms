3a97efba07030bfc5ad3688288269918aa574708
goddardjer <goddarje@oregonstate.edu>
2024-02-28 12:34:36 -0800

    try again

--- MovingCode.py
+++ MovingCode.py
@@ -14,6 +14,12 @@ from CameraCalibration.CalibrationConfig import *
 
 AK = ArmIK()
 
+coordinate = {
+        'red':   (-15 + 0.5, 12 - 0.5, 1.5),
+        'green': (-15 + 0.5, 6 - 0.5,  1.5),
+        'blue':  (-15 + 0.5, 0 - 0.5,  1.5),
+    }
+
 # Initial position setup
 def initMove():
     Board.setBusServoPulse(1, 500 - 50, 300)
@@ -25,24 +31,47 @@ rotation_angle = 0
 
 # Function to pick up a block at the specified coordinates
 def pick_up_block(x, y, z, color):
-    result = AK.setPitchRangeMoving((x, y - 2, 5), -90, -90, 0)
+    # Pick up the block
+    result = AK.setPitchRangeMoving((200, 200, 1.5), -90, -90, 0)
     if result == False:
         print("Unreachable")
     else:
         time.sleep(result[2]/1000)
         Board.setBusServoPulse(1, 500 - 280, 500)
-        servo2_angle = getAngle(x, y, rotation_angle)
+        servo2_angle = getAngle(200, 200, rotation_angle)
         Board.setBusServoPulse(2, servo2_angle, 500)
         time.sleep(0.8)
-        AK.setPitchRangeMoving((x, y, 2), -90, -90, 0, 1000)
+        AK.setPitchRangeMoving((200, 200, 2), -90, -90, 0, 1000)
         time.sleep(2)
         Board.setBusServoPulse(1, 500, 500)
         time.sleep(1)
-        Board.setBusServoPulse(2, 500, 500)
-        AK.setPitchRangeMoving((x, y, 12), -90, -90, 0, 1000)
-        time.sleep(1)
-        initMove()
-        time.sleep(1.5)
+
+    # Move to the target location
+    result = AK.setPitchRangeMoving((x, y, z + 10), -90, -90, 0)
+    if result == False:
+        print("Unreachable")
+    else:
+        time.sleep(result[2]/1000)
+
+    # Lower the block
+    result = AK.setPitchRangeMoving((x, y, z), -90, -90, 0)
+    if result == False:
+        print("Unreachable")
+    else:
+        time.sleep(result[2]/1000)
+        Board.setBusServoPulse(1, 500 - 280, 500)
+        time.sleep(0.8)
+
+    # Release the block
+    Board.setBusServoPulse(1, 500, 500)
+    time.sleep(1)
+
+    # Return to the initial position
+    Board.setBusServoPulse(2, 500, 500)
+    AK.setPitchRangeMoving((x, y, z + 12), -90, -90, 0, 1000)
+    time.sleep(1)
+    initMove()
+    time.sleep(1.5)
 
 if __name__ == '__main__':
     initMove()
