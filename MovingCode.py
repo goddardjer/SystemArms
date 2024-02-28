e9630e5fa7ef069e5ac67702480e3e5ddd361f72
goddardjer <goddarje@oregonstate.edu>
2024-02-28 12:25:15 -0800

    dgkl

--- MovingCode.py
+++ MovingCode.py
@@ -23,26 +23,24 @@ def initMove():
 
 # Function to pick up a block at the specified coordinates
 def pick_up_block(x, y, z, color):
-    set_rgb(color)
     result = AK.setPitchRangeMoving((x, y - 2, 5), -90, -90, 0)
     if result == False:
         print("Unreachable")
     else:
         time.sleep(result[2]/1000)
-        Board.setBusServoPulse(1, servo1 - 280, 500)
+        Board.setBusServoPulse(1, 500 - 280, 500)
         servo2_angle = getAngle(x, y, rotation_angle)
         Board.setBusServoPulse(2, servo2_angle, 500)
         time.sleep(0.8)
         AK.setPitchRangeMoving((x, y, 2), -90, -90, 0, 1000)
         time.sleep(2)
-        Board.setBusServoPulse(1, servo1, 500)
+        Board.setBusServoPulse(1, 500, 500)
         time.sleep(1)
         Board.setBusServoPulse(2, 500, 500)
         AK.setPitchRangeMoving((x, y, 12), -90, -90, 0, 1000)
         time.sleep(1)
         initMove()
         time.sleep(1.5)
-        set_rgb('None')
 
 if __name__ == '__main__':
     initMove()
