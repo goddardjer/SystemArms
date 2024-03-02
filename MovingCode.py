c0b75218991bd73d59f1e4067c4f882ddb60db87
goddardjer <goddarje@oregonstate.edu>
2024-03-02 13:51:12 -0800

    dgzfs

--- MovingCode.py
+++ MovingCode.py
@@ -24,6 +24,7 @@ coordinate = {
 def initMove():
     Board.setBusServoPulse(1, 500 - 50, 300)
     Board.setBusServoPulse(2, 500, 500)
+    # Set the initial position of the arm
     AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
 
 
@@ -31,12 +32,13 @@ rotation_angle = 0
 
 # Function to pick up a block at the specified coordinates
 def pick_up_block(x, y, z, color):
-    # Pick up the block
     Board.setBusServoPulse(1, 500 - 280, 500)
+    # Move to the block
     result = AK.setPitchRangeMoving((-2, 18, 1.5), -90, -90, 1000)
     if result == False:
         print("Unreachable 0")
     else:
+        print("Moving to the block...")
         time.sleep(result[2]/1000)
         servo2_angle = getAngle(200, 200, rotation_angle)
         Board.setBusServoPulse(2, servo2_angle, 500)
@@ -46,7 +48,7 @@ def pick_up_block(x, y, z, color):
         Board.setBusServoPulse(1, 500, 500)
         time.sleep(1)
 
-    # Move to the target location
+    # Move to the final location for the plock to be placed
     result = AK.setPitchRangeMoving((x, y, z + 10), -90, -90, 1000)
     if result == False:
         print("Unreachable 1")
@@ -54,24 +56,24 @@ def pick_up_block(x, y, z, color):
         time.sleep(result[2]/1000)
 
     # Lower the block
-    result = AK.setPitchRangeMoving((x, y, z), -90, -90, 0)
+    result = AK.setPitchRangeMoving((x, y, z), -90, -90, 1000)
     if result == False:
         print("Unreachable 2")
     else:
+        # Release the block
         time.sleep(result[2]/1000)
         Board.setBusServoPulse(1, 500 - 280, 500)
         time.sleep(0.8)
 
-    # Release the block
-    Board.setBusServoPulse(1, 500, 500)
-    time.sleep(1)
-
     # Return to the initial position
-    Board.setBusServoPulse(2, 500, 500)
     AK.setPitchRangeMoving((x, y, z + 12), -90, -90, 0, 1000)
     time.sleep(1)
     initMove()
     time.sleep(1.5)
+
+    # Release close the hand
+    Board.setBusServoPulse(1, 500, 500)
+    time.sleep(1)
 
 if __name__ == '__main__':
     initMove()
