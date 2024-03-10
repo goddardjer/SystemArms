08a046b5abfc6d930b64d32d2909fcb63dd133f7
goddardjer <goddarje@oregonstate.edu>
2024-03-10 01:09:03 -0800

    dhsjfk sdhfj ashdlsdfkls

--- Keyboard_to_cords.py
+++ Keyboard_to_cords.py
@@ -8,6 +8,56 @@ from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
 
+##########################################################
+
+class CircleDetector:
+    def __init__(self):
+        self.target_color = 'black'
+        self.camera = Camera.Camera()
+        self.camera.camera_open()
+        self.size = (640, 480)
+
+    def process_frame(self, frame):
+        frame_resize = cv2.resize(frame, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)
+        return frame_lab
+
+    def draw_circles(self, frame, frame_lab):
+        frame_mask = cv2.inRange(frame_lab, color_range[self.target_color][0], color_range[self.target_color][1])
+        opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
+        closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
+        circles = cv2.HoughCircles(closed, cv2.HOUGH_GRADIENT, dp=1, minDist=50, param1=70, param2=20, minRadius=15, maxRadius=400)
+
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                center_x, center_y = convertCoordinate(i[0], i[1], self.size)
+                cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+                cv2.putText(frame, self.target_color, (i[0], i[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1)
+                cv2.putText(frame, f'x: {center_x:.2f}, y: {center_y:.2f}', (i[0], i[1] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 1)
+
+    def run(self):
+        while True:
+            img = self.camera.frame
+            if img is not None:
+                frame = img.copy()
+                frame_lab = self.process_frame(frame)
+                self.draw_circles(frame, frame_lab)
+                cv2.imshow('Frame', frame)
+                key = cv2.waitKey(1)
+                if key == 27:  # ESC key to break
+                    break
+
+        self.camera.camera_close()
+        cv2.destroyAllWindows()
+
+
+###########################################################
+
+
 class Moving:
     def __init__(self):
         self.AK = ArmIK()
@@ -20,30 +70,42 @@ class Moving:
         Board.setBusServoPulse(2, 500, 500)
         self.AK.setPitchRangeMoving((0, 8, 24), -90, -90, 1500)
 
+    def open_hand(self):
+        Board.setBusServoPulse(1, 500 - 280, 500)
+        time.sleep(0.8)
+
+    def close_hand(self):
+        Board.setBusServoPulse(1, 500, 500)
+        time.sleep(1)
+
+    def move_horozontal(self, increment):
+        self.servo_horizontal += increment
+        Board.setBusServoPulse(3, self.servo_horizontal, 500)
+
+    def move_vertical(self, increment):
+        self.servo_vertical += increment
+        Board.setBusServoPulse(6, self.servo_vertical, 500)
+
+    def calculate_pulse(self, x, y):
+        pulse_x = int(500 - (x / 0.29))
+        pulse_y = int(500 - (y / 0.29))
+        return pulse_x, pulse_y
+
     def move_arm_with_keyboard(self):
         while True:
             key = input("Enter a key: ")
             if key == 'w':
-                self.y += 2
+                self.move_vertical(10)
             elif key == 's':
-                self.y -= 2
+                self.move_vertical(-10)
             elif key == 'a':
-                self.x -= 2
+                self.move_horozontal(-10)
             elif key == 'd':
-                self.x += 5
-            elif key == 'q':
-                self.z -= 2
-            elif key == 'e':
-                self.z += 2
+                self.move_horozontal(10)
             elif key == 'x':
                 break
 
-            result = self.AK.setPitchRangeMoving((self.x, self.y, self.z), -90, -90, 1000)
-            if result:
-                print(f'x: {self.x}, y: {self.y}, z: {self.z}')
-                time.sleep(result[2]/1000)
 
 if __name__ == '__main__':
     moving = Moving()
     moving.initMove()
-    moving.move_arm_with_keyboard()
