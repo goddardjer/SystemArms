1c66b9ad4f5619a58305f132e1b3b9bb2d3b22c2
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-03-15 19:16:15 -0700

    I am not a real roboticist

--- A_bioE_did_this.py
+++ A_bioE_did_this.py
@@ -7,6 +7,11 @@ from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
+import numpy as np
+import time
+import threading
+
+##########################################################
 
 class CircleDetector:
     def __init__(self):
@@ -14,6 +19,8 @@ class CircleDetector:
         self.camera = Camera.Camera()
         self.camera.camera_open()
         self.size = (640, 480)
+        self.center_x = None
+        self.center_y = None
 
     def process_frame(self, frame):
         frame_resize = cv2.resize(frame, self.size, interpolation=cv2.INTER_NEAREST)
@@ -27,15 +34,18 @@ class CircleDetector:
         closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
         circles = cv2.HoughCircles(closed, cv2.HOUGH_GRADIENT, dp=1, minDist=50, param1=70, param2=20, minRadius=15, maxRadius=400)
 
-
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
-                center_x, center_y = convertCoordinate(i[0], i[1], self.size)
+                self.center_x, self.center_y = convertCoordinate(i[0], i[1], self.size)
                 cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
                 cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
                 cv2.putText(frame, self.target_color, (i[0], i[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1)
-                cv2.putText(frame, f'x: {center_x:.2f}, y: {center_y:.2f}', (i[0], i[1] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 1)
+                cv2.putText(frame, f'x: {self.center_x:.2f}, y: {self.center_y:.2f}', (i[0], i[1] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 1)
+
+    def get_circle_center(self):
+        return self.center_x, self.center_y
+
 
     def run(self):
         while True:
@@ -49,9 +59,63 @@ class CircleDetector:
                 if key == 27:  # ESC key to break
                     break
 
+                if self.center_x is not None and self.center_y is not None:
+                    self.camera.camera_close()
+                    cv2.destroyAllWindows()
+                    return self.center_x, self.center_y
+
         self.camera.camera_close()
         cv2.destroyAllWindows()
 
+###########################################################
+
+class Interpreter:
+    def __init__(self):
+        pass
+
+    def interpolate_servoH(self, CV_H):
+        CV_H_values = [-8, 0, 8]
+        Servo_H_values = [540, 500, 450]
+        return np.interp(CV_H, CV_H_values, Servo_H_values)
+
+    def interpolate_servoV(self, CV_V):
+        CV_V_values = [16.5, 20.25, 24]
+        Servo_V_values = [100, 115, 130]
+        return np.interp(CV_V, CV_V_values, Servo_V_values)
+
+    def run(self, center_x, center_y):
+        ServoH = self.interpolate_servoH(center_x)
+        ServoV = self.interpolate_servoV(center_y)
+        return ServoH, ServoV
+
+
+class Moving:
+    def __init__(self):
+        self.servo_horizontal = 500  # Initial position for horizontal servo
+        self.servo_vertical = 100    # Initial position for vertical servo
+
+    def open_hand(self):
+        Board.setBusServoPulse(1, 500 - 450, 500)
+        time.sleep(0.8)
+
+    def fire(self):
+        self.open_hand()
+
+    def move_to_target(self, servo_horizontal, servo_vertical):
+        Board.setBusServoPulse(6, servo_horizontal, 500)
+        Board.setBusServoPulse(3, servo_vertical, 500)
+
+def run_interpreter_and_move(interpreter, moving, circledetect):
+    center_x, center_y = circledetect.run()
+    servoH, servoV = interpreter.run(center_x, center_y)
+    moving.move_to_target(servoH, servoV)
+    moving.fire()
+
 if __name__ == '__main__':
-    detector = CircleDetector()
-    detector.run()
+    circledetect = CircleDetector()
+    interpreter = Interpreter()
+    moving = Moving()
+
+    run_interpreter_and_move(interpreter, moving, circledetect)
+
+    print("Done")
